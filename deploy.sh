f612fb7b7e5bd59fbbf8085395dbab093824fd92
Luigi Nigrelli <luiginigrelli@me.com>
2021-01-31 14:08:30 +0100

    deploy logs

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
+echo "BUILD IMAGES"
 docker build -t lgngr/multi-client:latest -t lgngr/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t lgngr/multi-server:latest -t lgngr/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t lgngr/multi-worker:latest -t lgngr/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
+echo "PUSH IMAGES"
 docker push lgngr/multi-client:latest 
 docker push lgngr/multi-client:$SHA
 docker push lgngr/multi-server:latest 
@@ -9,8 +11,10 @@ docker push lgngr/multi-server:$SHA
 docker push lgngr/multi-worker:latest 
 docker push lgngr/multi-worker:$SHA
 
+echo "APPLY K8S"
 kubectl apply -f k8s
 
+echo "SET IMAGES"
 kubectl set image deployments/client-deployment server=lgngr/multi-client:$SHA
 kubectl set image deployments/server-deployment client=lgngr/multi-server:$SHA
 kubectl set image deployments/worker-deployment worker=lgngr/multi-worker:$SHA
